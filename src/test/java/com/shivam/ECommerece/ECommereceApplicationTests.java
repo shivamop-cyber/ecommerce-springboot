57279657d40c7dc02f62488ce91796d2246c6091
Shivam Singh <shivamsingh00141@gmail.com>
2023-12-10 12:43:11 +0530

    Initial spring project for Ecommerce

---
+++ src/test/java/com/shivam/ECommerece/ECommereceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shivam.ECommerece;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommereceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
